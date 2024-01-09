7ffb1b8287f56726fc6eb1d1f335f7e032819458
kongthedog <2da2da49@gmail.com>
2024-01-08 17:25:33 -0800

    crerate dinoshop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
+import data from './data';
 
 function App() {
+
+  let [dino] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">DinoShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* main bg */}
+      <div className='main-bg'></div>
+
+      {/* main layout */}
+      <Container>
+        <Row>
+          <Col className='main-layout'>
+            <img src={process.env.PUBLIC_URL + '/img/dinosaur_1.jpg'}/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col className='main-layout'>
+            <img src={process.env.PUBLIC_URL + '/img/dinosaur_2.jpg'}/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col className='main-layout'>
+            <img src={process.env.PUBLIC_URL + '/img/dinosaur_3.jpg'}/>
+            <h4></h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      id : 0,
+      title : "Book",
+      content : "Dinosaur Bingo Book",
+      price : 60
+    },
+  
+    {
+      id : 1,
+      title : "Figure",
+      content : "Figure of Ankylosaurus",
+      price : 120
+    },
+  
+    {
+      id : 2,
+      title : "Poster",
+      content : "Poster of Brachiosaurus",
+      price : 35
+    }
+  ] 
+
+  export default data;
